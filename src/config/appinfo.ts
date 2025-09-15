6be20662bf2024e0c824b6314ccf3ae03357f922
NafeesIS <nafeesshawon555@gmail.com>
2025-09-15 13:40:58 +0600

    added slash in api base path

--- src/config/appinfo.ts
+++ src/config/appinfo.ts
@@ -4,6 +4,6 @@ export const appInfo = {
       appName: "Course Mate",
       apiDomain: "http://localhost:4000",   // backend
       websiteDomain: "http://localhost:3000", // frontend
-      apiBasePath: "/auth",
+      apiBasePath: "/api/v1/auth",
       websiteBasePath: "/auth",
     };
